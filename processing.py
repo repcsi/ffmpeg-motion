3b02e36beba4f32704e34708149c3d982ed8f62f
repcsi <repcsike@gmail.com>
2019-10-23 10:07:17 +0200

    Replaced os.move with shutil.move.

--- processing.py
+++ processing.py
@@ -1,11 +1,11 @@
-import os
 import ffmpeg
 import tempfile
+import shutil
 
 def convert(inFile):
     basename, extension = inFile.split(".")
     tmpFile = tempfile.mktemp()
     #outFile = basename+"-temp."+extension
     outFile = tmpFile
-    ffmpeg.input(inFile).output(outFile, pix_fmt="yuv420p",vcodec="libx264",crf="20").run()
-    os.rename(outFile, inFile)
+    ffmpeg.input(inFile).output(outFile, format="mp4", pix_fmt="yuv420p", vcodec="libx264", crf="20").run()
+    shutil.move(outFile, inFile)
